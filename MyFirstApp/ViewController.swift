47f0386b6042f64ffb1523998bf59e2ccfe9890c
JustATechie <JustTheTechie@gmail.com>
2019-10-25 20:40:39 -0700

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Louis DiBernardo on 10/25/19.
+//  Copyright © 2019 Louis DiBernardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
